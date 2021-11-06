87e5e73970be1bec4fe740cd3a5401c6fc89bd8a
KIM DONG IL <cappu159@gmail.com>
2021-11-06 15:24:23 +0900

    init app.js

---
+++ tracalorie/app.js
@@ -0,0 +1,12 @@
+// Storage Controller
+
+// Item Controller
+const itemCtrl = (function () {
+  // Item Constructor
+})();
+
+// UI Controller
+const UICtrl = (function () {})();
+
+// App Controller
+const App = (function (itemCtrl, UICtrl) {})(itemCtrl, UICtrl);
